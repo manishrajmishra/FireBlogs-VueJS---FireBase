8495128c6f4805523d37caf2a299d6bbe6de61d4
Manish Raj <rajmanish6851@gmail.com>
2021-08-01 08:12:20 +0530

    Create Navigation.vue
    Created Navigation Component for Desktop

---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+<header>
+    <nav class="container">
+        <div class="branding">
+            <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
+        </div>
+        <div class="nav-links">
+            <ul>
+                <router-link class="link" to="#">Home</router-link>
+                <router-link class="link" to="#">Blogs</router-link>
+                <router-link class="link" to="#">Create Post</router-link>
+                <router-link class="link" to="#">Login</router-link>
+                <router-link class="link" to="#">Register</router-link>
+            </ul>
+        </div>
+    </nav>
+</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+</style>
