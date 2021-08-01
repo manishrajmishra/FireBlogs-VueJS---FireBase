bad6e59e43ef4a1bea6d3d2e07d648c17c47c4e5
Manish Raj <rajmanish6851@gmail.com>
2021-08-01 08:19:44 +0530

    Update Navigation.vue
    Add Navigation for Mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,21 @@
 <template>
-<header>
-    <nav class="container">
-        <div class="branding">
-            <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
-        </div>
-        <div class="nav-links">
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
+            </div>
+            <div class="nav-links">
+                <ul>
+                    <router-link class="link" to="#">Home</router-link>
+                    <router-link class="link" to="#">Blogs</router-link>
+                    <router-link class="link" to="#">Create Post</router-link>
+                    <router-link class="link" to="#">Login</router-link>
+                    <router-link class="link" to="#">Register</router-link>
+                </ul>
+            </div>
+        </nav>
+        <menuIcon/>
+        <transition name="mobile-nav">
             <ul>
                 <router-link class="link" to="#">Home</router-link>
                 <router-link class="link" to="#">Blogs</router-link>
@@ -12,14 +23,17 @@
                 <router-link class="link" to="#">Login</router-link>
                 <router-link class="link" to="#">Register</router-link>
             </ul>
-        </div>
-    </nav>
-</header>
+        </transition>
+    </header>
 </template>
 
 <script>
+import menuIcon from '../assets/Icons/bars-regular.svg'
 export default {
-
+    name: 'navigation',
+    componets: {
+        menuIcon
+    }
 }
 </script>
 <style lang="scss" scoped>
