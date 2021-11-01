c498243a3384a7863b396328c28aba5bfa80fe48
Manish Raj <rajmanish6851@gmail.com>
2021-11-02 00:11:09 +0530

    Style: Transition effect for mobile navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -136,4 +136,19 @@ nav{
          color: #fff;
         }
     }
+
+  .mobile-nav-enter-active,
+  .mobile-nav-leave-active {
+    transition: all 5s ease;
+  }
+  .mobile-nav-enter {
+    transform: translateX(-250px);
+  }
+  .mobile-nav-enter-to {
+    transform: translateX(0);
+  }
+  .mobile-nav-leave-to {
+    transform: translateX(-250px);
+  }
+    
 </style>
