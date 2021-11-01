1037bfdee727c0b4a8c2fbc7bd3de69cb89d306b
Manish Raj <rajmanish6851@gmail.com>
2021-11-02 01:02:01 +0530

    Added BlogPost Component and dummy data for blogspost

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="blog-wrapper">
+        <div class="blog-content">
+            <div>
+                <h2 v-if="post.welcomeScreen">{{post.title}}</h2>
+                <h2 v-else>{{post.title}}</h2>
+                <p v-if="post.welcomeScreen">{{post.blogPost}}</p>
+                <p class="content-preview" v-else>{{post.blogHTML}}</p>
+                <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
+                    LogIn/Register<Arrow class="arrow arrow-light" />
+                </router-link>
+                <router-link class="link link-light" v-else to="#">
+                    View The Post<Arrow class="arrow" />
+                </router-link>
+            </div>
+            <div class="blog-photo">
+                <img v-if="post.welcomeScreen" :src="require(`../assets/blogPhotos/${post.photo}.jpg`)" alt=""/>
+                <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt=""/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Arrow from '../assets/Icons/arrow-right-light.svg'
+export default {
+    name: 'blogPost',
+    components: {
+        Arrow
+    },
+    props: {
+        post : {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
