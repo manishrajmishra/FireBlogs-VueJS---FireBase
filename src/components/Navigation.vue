43a4738da16fe108edee6f40076ba6ddeb7961d0
Manish Raj <rajmanish6851@gmail.com>
2021-11-01 23:34:53 +0530

    Added toggle Effect in Mobile View

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
                 <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
             </div>
             <div class="nav-links">
-                <ul>
+                <ul v-show="!mobile">
                     <router-link class="link" to="#">Home</router-link>
                     <router-link class="link" to="#">Blogs</router-link>
                     <router-link class="link" to="#">Create Post</router-link>
@@ -14,9 +14,9 @@
                 </ul>
             </div>
         </nav>
-        <menuIcon class="menu-icon"/>
+        <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
         <transition class="mobile-nav">
-            <ul class="mobile-nav">
+            <ul class="mobile-nav" v-show="mobileNav">
                 <router-link class="link" to="#">Home</router-link>
                 <router-link class="link" to="#">Blogs</router-link>
                 <router-link class="link" to="#">Create Post</router-link>
@@ -33,6 +33,32 @@ export default {
     name: 'navigation',
     components: {
         menuIcon
+    },
+    data () {
+      return {
+        mobile: null,
+        mobileNav: null,
+        windowWidth: null
+      }
+    },
+    created(){
+      window.addEventListener('resize', this.checkScreen)
+      this.checkScreen()
+    },
+    methods: {
+      checkScreen(){
+        this.windowWidth = window.innerWidth
+        if(this.windowWidth <= 750){
+          this.mobile = true
+          return
+        }
+        this.mobile = false
+        this.mobileNav = false
+        return
+      },
+      toggleMobileNav(){
+        this.mobileNav = !this.mobileNav
+      }
     }
 }
 </script>
