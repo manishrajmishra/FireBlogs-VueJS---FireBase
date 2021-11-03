d23aced3049bb83a1a9925d8ad405ed72c69e46a
Manish Raj <rajmanish6851@gmail.com>
2021-11-03 08:03:56 +0530

    Style: Designed Home Page

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="blog-wrapper">
+    <div class="blog-wrapper no-user">
         <div class="blog-content">
             <div>
                 <h2 v-if="post.welcomeScreen">{{post.title}}</h2>
@@ -9,14 +9,14 @@
                 <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
                     LogIn/Register<Arrow class="arrow arrow-light" />
                 </router-link>
-                <router-link class="link link-light" v-else to="#">
+                <router-link class="link" v-else to="#">
                     View The Post<Arrow class="arrow" />
                 </router-link>
             </div>
-            <div class="blog-photo">
-                <img v-if="post.welcomeScreen" :src="require(`../assets/blogPhotos/${post.photo}.jpg`)" alt=""/>
-                <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt=""/>
-            </div>
+        </div>
+        <div class="blog-photo">
+            <img v-if="post.welcomeScreen" :src="require(`../assets/blogPhotos/${post.photo}.jpg`)" alt=""/>
+            <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt=""/>
         </div>
     </div>
 </template>
@@ -39,4 +39,119 @@ export default {
 
 <style lang="scss" scoped>
 
+.blog-wrapper{
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    @media(min-width: 700px){
+        min-height: 650px;
+        min-height: 650px;
+        flex-direction: row;
+    }
+
+    .blog-content{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 4;
+        order: 2;
+        @media(min-width: 700px){
+            order: 1;
+        }
+        @media(min-width: 800px) {
+            flex: 3;
+        }
+
+        div{
+            max-width: 375px;
+            padding: 72px 24px;
+            @media(min-width: 700px){
+                padding: 0 24px;
+            }
+
+            h2{
+                font-size: 32px;
+                font-weight: 300;
+                text-transform: uppercase;
+                margin-bottom: 24px;
+                @media(min-width: 700px){
+                    font-size: 40px;
+                }
+            }
+
+            p{
+                font-size: 15px;
+                font-weight: 300;
+                line-height: 1.7;
+            }
+
+            .content-preview{
+                font-size: 13px;
+                max-height: 24px;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                width: 250px;
+            }
+
+            .link{
+                display: inline-flex;
+                align-items: center;
+                margin-top: 32px;
+                padding-bottom: 4px;
+                border-bottom: 1px solid transparent;
+                transition: .5 ease-in all;
+
+                &:hover{
+                    border-bottom-color: #303030
+                }
+            }
+
+            .link-light {
+                &:hover{
+                    border-bottom-color: #ffff;
+                }
+            }
+        }
+    }
+
+    .blog-photo{
+        order: 1;
+        flex: 3;
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+        @media(min-width: 700px){
+            order: 2;
+        }
+
+        @media(min-width: 800px){
+            flex: 4;
+        }
+
+        img{
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+
+        }
+    }
+
+    &:nth-child(even){
+        .blog-content{
+            order: 2;
+        }
+        .blog-photo{
+            order: 1;
+        }
+    }
+}
+
+.no-user:first-child{
+    .blog-content{
+        background-color: #303030;
+        color: #ffff
+    }
+}
 </style>
