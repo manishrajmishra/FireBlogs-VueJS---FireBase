b97b93afc9cd3b1ba2ff60b5b67c10d46a1c65b6
Manish Raj <rajmanish6851@gmail.com>
2021-11-05 21:14:33 +0530

    Maked Use of Store for sampleblogcards

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sampleBlogCards: [
+        { blogTitle: 'Blog Card #1', blogCoverPhoto: 'stock-1', blogDate: 'Nov 1 2021' },
+        { blogTitle: 'Blog Card #2', blogCoverPhoto: 'stock-2', blogDate: 'Nov 2 2021' },
+        { blogTitle: 'Blog Card #3', blogCoverPhoto: 'stock-3', blogDate: 'Nov 3 2021' },
+        { blogTitle: 'Blog Card #4', blogCoverPhoto: 'stock-4', blogDate: 'Nov 4 2021' }
+      ]
   },
   mutations: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,13 +46,12 @@ export default {
           blogHTML: 'This is a filler blog post title',
           blogCoverPhoto: 'designed-for-everyone'
         },
-      ],
-      sampleBlogCards: [
-        { blogTitle: 'Blog Card #1', blogCoverPhoto: 'stock-1', blogDate: 'Nov 1 2021' },
-        { blogTitle: 'Blog Card #2', blogCoverPhoto: 'stock-2', blogDate: 'Nov 2 2021' },
-        { blogTitle: 'Blog Card #3', blogCoverPhoto: 'stock-3', blogDate: 'Nov 3 2021' },
-        { blogTitle: 'Blog Card #4', blogCoverPhoto: 'stock-4', blogDate: 'Nov 4 2021' }
       ]
+    }
+  },
+  computed: {
+    sampleBlogCards() {
+      return this.$store.state.sampleBlogCards
     }
   }
 };
