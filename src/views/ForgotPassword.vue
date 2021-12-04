3699304535ea36109a60d869527f0334b0892a66
Manish Raj <rajmanish6851@gmail.com>
2021-12-04 08:43:26 +0530

    Created and styled Forgot Password Page

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,10 +1,40 @@
 <template>
-    <div></div>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <p class="login-register">
+          Back to
+          <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
+        </p>
+        <h2>Reset Password</h2>
+        <p>Forgot your passowrd? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
+
 <script>
+import email from "../assets/Icons/envelope-regular.svg";
 export default {
-    
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: ""
+    };
+  },
+  components: {
+    email
+  },
 }
 </script>
 
