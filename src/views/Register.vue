f7d4459438bede109e5c2767aee5ee90be610d79
Manish Raj <rajmanish6851@gmail.com>
2021-12-04 08:29:57 +0530

    Created Register Page and Styled it

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,63 @@
 <template>
-    <div></div>
+    <div class="form-wrap">
+        <form class="register">
+          <p class="login-register">
+            Don't have an account ?
+            <router-link class="route-link" :to="{ name: 'Register' }">Register</router-link>
+          </p>
+          <h2>Login to FireBlogs</h2>
+          <div class="inputs">
+            <div class="input">
+              <input type="text" placeholder="First Name" v-model="firstName" />
+              <user class="icon" />
+            </div>
+            <div class="input">
+              <input type="text" placeholder="Last Name" v-model="lastName" />
+              <user class="icon" />
+            </div>
+            <div class="input">
+              <input type="text" placeholder="UserName" v-model="userName" />
+              <user class="icon" />
+            </div>
+            <div class="input">
+              <input type="email" placeholder="Email" v-model="email" />
+              <email class="icon" />
+            </div>
+            <div class="input">
+              <input type="password" placeholder="Password" v-model="password" />
+              <password class="icon" />
+            </div>
+          </div>
+          <router-link class="forgot-password" :to="{ name: 'ForgotPassword' }">Forgot your Password</router-link>
+          <button>Sign Up</button>
+          <div class="angle"></div>
+        </form>
+        <div class="background"></div>
+    </div>
 </template>
 
 <script>
+import email from "../assets/Icons/envelope-regular.svg";
+import password from "../assets/Icons/lock-alt-solid.svg";
+import user from "../assets/Icons/user-alt-light.svg";
 export default {
-    
+  name: "Register",
+  components: {
+    email,
+    password,
+    user,
+  },
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      email: '',
+      password: '',
+      error: null,
+      errorMsg: ''
+    }
+  }
 }
 </script>
 
