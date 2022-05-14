4adc77db5b503603b7ad791a6382d2714e064afa
manishrajmishra <rajmanish6851@gmail.com>
2022-05-14 09:14:25 +0530

    Added get Post action in Store

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,33 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import db from "../firebase/firebaseInit";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     sampleBlogCards: [
-        { blogTitle: 'Blog Card #1', blogCoverPhoto: 'stock-1', blogDate: 'Nov 1 2021' },
-        { blogTitle: 'Blog Card #2', blogCoverPhoto: 'stock-2', blogDate: 'Nov 2 2021' },
-        { blogTitle: 'Blog Card #3', blogCoverPhoto: 'stock-3', blogDate: 'Nov 3 2021' },
-        { blogTitle: 'Blog Card #4', blogCoverPhoto: 'stock-4', blogDate: 'Nov 4 2021' }
-      ],
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-1",
+        blogDate: "Nov 1 2021",
+      },
+      {
+        blogTitle: "Blog Card #2",
+        blogCoverPhoto: "stock-2",
+        blogDate: "Nov 2 2021",
+      },
+      {
+        blogTitle: "Blog Card #3",
+        blogCoverPhoto: "stock-3",
+        blogDate: "Nov 3 2021",
+      },
+      {
+        blogTitle: "Blog Card #4",
+        blogCoverPhoto: "stock-4",
+        blogDate: "Nov 4 2021",
+      },
+    ],
+    blogPosts: [],
     postLoaded: null,
     blogHTML: "Write your blog title here...",
     blogTitle: "",
@@ -54,22 +71,24 @@ export default new Vuex.Store({
     OPEN_PHOTO_PREVIEW(state) {
       state.blogPhotoPreview = !state.blogPhotoPreview;
     },
-    TOGGLE_EDIT_POST(state, payload){
-      state.editPost = payload
-      console.log(state.editPost)
+    TOGGLE_EDIT_POST(state, payload) {
+      state.editPost = payload;
+      console.log(state.editPost);
     },
-    UPDATE_USER(state, payload){
+    UPDATE_USER(state, payload) {
       state.user = payload;
     },
-    SET_PROFILE_INFO(state, payload){
+    SET_PROFILE_INFO(state, payload) {
       state.profileId = payload.id;
       state.profileEmail = payload.data().email;
       state.profileFirstName = payload.data().firstName;
       state.profileLastName = payload.data().lastName;
       state.profileUsername = payload.data().userName;
     },
-    SET_PROFILE_INITIALS(state){
-      state.profileInitials = state.profileFirstName.match(/(\b\S)?/g).join("") + state.profileLastName.match(/(\b\S)?/g).join("");
+    SET_PROFILE_INITIALS(state) {
+      state.profileInitials =
+        state.profileFirstName.match(/(\b\S)?/g).join("") +
+        state.profileLastName.match(/(\b\S)?/g).join("");
     },
     CHANGE_FIRST_NAME(state, payload) {
       state.profileFirstName = payload;
@@ -82,12 +101,14 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    async getCurrentUser({ commit }){
-      const dataBase = await db.collection("users").doc(firebase.auth().currentUser.uid)
-      const dbResults = await dataBase.get()
-      commit("SET_PROFILE_INFO", dbResults)
-      commit('SET_PROFILE_INITIALS')
-      console.log(dbResults)
+    async getCurrentUser({ commit }) {
+      const dataBase = await db
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid);
+      const dbResults = await dataBase.get();
+      commit("SET_PROFILE_INFO", dbResults);
+      commit("SET_PROFILE_INITIALS");
+      console.log(dbResults);
     },
     async updateUserSettings({ commit, state }) {
       const dataBase = await db.collection("users").doc(state.profileId);
@@ -98,7 +119,23 @@ export default new Vuex.Store({
       });
       commit("SET_PROFILE_INITIALS");
     },
+    async getPost({ state }) {
+      const dataBase = await db.collection("blogPosts").orderBy("date", "desc");
+      const dbResults = await dataBase.get();
+      dbResults.forEach((doc) => {
+        if (!state.blogPosts.some((post) => post.blogID === doc.id)) {
+          const data = {
+            blogID: doc.data().blogID,
+            blogHTML: doc.data().blogHTML,
+            blogCoverPhoto: doc.data().blogCoverPhoto,
+            blogTitle: doc.data().blogTitle,
+            blogDate: doc.data().date,
+          };
+          state.blogPosts.push(data);
+        }
+      });
+      state.postLoaded = true;
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
